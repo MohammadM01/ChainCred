30c3fd45ae7bf163e41acfd31a32ca94b4c7c438
MohammadM01 <mullamohammad123@gmail.com>
2025-08-25 15:21:49 +0530

    Final deploy ready..🤩

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
   assetsInclude: ['**/*.splinecode'],
+  preview: {
+    allowedHosts: ['chaincred-frontend.onrender.com'],
+    host: true,        // ensures it binds to 0.0.0.0
+    port: 4173         // default vite preview port (Render replaces with $PORT)
+  }
 })
